13a8c2b41786adeb60735ec12fae22a8b9c4b40a
hamed <hamed.alizadegan@gmail.com>
2023-12-13 19:23:24 +0330

    fixed: urls

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='home')
+    # path('', views.IndexView.as_view(), name='home')
+    path('header_image/', views.HeaderImageView.as_view(), name='header_image'),
+    path('course_category/', views.CourseCategoryView.as_view(), name='course_category'),
+    path('course_subcategory/', views.CourseSubcategoryView.as_view(), name='course_subcategory'),
+    path('sample_exercise/', views.SampleExerciseView.as_view(), name='sample_exercise'),
+    path('course/', views.CourseView.as_view(), name='course'),
+    path('feedback/', views.FeedbackView.as_view(), name='feedback'),
+    path('blog/', views.BlogView.as_view(), name='blog'),
 ]
 
--- home/views.py
+++ home/views.py
@@ -10,30 +10,101 @@ from blog.serializers import BlogSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
-class IndexView(APIView):
+# class IndexView(APIView):
+#     # authentication_classes = [JWTAuthentication]
+#
+#     def get(self, request):
+#         header_image = HeaderImageModel.objects.all()
+#         ser_header_image = HeaderImageSerializer(instance=header_image, many=True)
+#
+#         course_category = CourseCategoryModel.objects.all()
+#         ser_course_category = CourseCategorySerializer(instance=course_category, many=True)
+#
+#         course_subcategory = CourseSubCategoryModel.objects.all()
+#         ser_course_subcategory = CourseSubCategorySerializer(instance=course_subcategory, many=True)
+#
+#         sample_exercise = SampleExerciseModel.objects.all()
+#         ser_sample_exercise = SampleExerciseSerializer(instance=sample_exercise, many=True)
+#
+#         course = CourseModel.objects.all()
+#         ser_course = CourseSerializer(instance=course, many=True)
+#
+#         feedback = FeedbackModel.objects.all()
+#         ser_feedback = FeedbackSerializer(instance=feedback, many=True)
+#
+#         blog = BlogModel.objects.all()
+#         ser_blog = BlogSerializer(instance=blog, many=True)
+#
+#         return Response(data=(ser_header_image.data, ser_course_category.data, ser_course_subcategory.data,
+#                               ser_course.data, ser_sample_exercise.data, ser_feedback.data, ser_blog.data))
+#
+
+class HeaderImageView(APIView):
     # authentication_classes = [JWTAuthentication]
 
     def get(self, request):
         header_image = HeaderImageModel.objects.all()
         ser_header_image = HeaderImageSerializer(instance=header_image, many=True)
 
+        return Response(data=ser_header_image.data)
+
+
+class CourseCategoryView(APIView):
+    # authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
         course_category = CourseCategoryModel.objects.all()
         ser_course_category = CourseCategorySerializer(instance=course_category, many=True)
 
+        return Response(data=ser_course_category.data)
+
+
+class CourseSubcategoryView(APIView):
+    # authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
         course_subcategory = CourseSubCategoryModel.objects.all()
         ser_course_subcategory = CourseSubCategorySerializer(instance=course_subcategory, many=True)
 
+        return Response(data=ser_course_subcategory.data)
+
+
+class SampleExerciseView(APIView):
+    # authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
         sample_exercise = SampleExerciseModel.objects.all()
         ser_sample_exercise = SampleExerciseSerializer(instance=sample_exercise, many=True)
+
+        return Response(data=ser_sample_exercise.data)
+
+
+class CourseView(APIView):
+    # authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
 
         course = CourseModel.objects.all()
         ser_course = CourseSerializer(instance=course, many=True)
 
+        return Response(data=ser_course.data)
+
+
+class FeedbackView(APIView):
+    # authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
         feedback = FeedbackModel.objects.all()
         ser_feedback = FeedbackSerializer(instance=feedback, many=True)
 
+        return Response(data=ser_feedback.data)
+
+
+class BlogView(APIView):
+    # authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
         blog = BlogModel.objects.all()
         ser_blog = BlogSerializer(instance=blog, many=True)
 
-        return Response(data=(ser_header_image.data, ser_course_category.data, ser_course_subcategory.data,
-                              ser_course.data, ser_sample_exercise.data, ser_feedback.data, ser_blog.data))
+        return Response(data=ser_blog.data)

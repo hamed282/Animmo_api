358fdf6a1964f54b0dfadebda25fccc011dfccca
hamed <hamed.alizadegan@gmail.com>
2024-02-12 18:51:51 +0330

    category icon updated

--- course/models.py
+++ course/models.py
@@ -6,6 +6,7 @@ class CourseCategoryModel(models.Model):
     category = models.CharField(max_length=50)
     slug = models.SlugField(max_length=100, unique=True)
     icon = models.ImageField(upload_to='images/icon/category/')
+    hovered_icon = models.ImageField(upload_to='images/icon/category/')
     class_des = models.CharField(max_length=20, default='course_category')
 
     def __str__(self):

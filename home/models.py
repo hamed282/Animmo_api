639707eeca982487790d7c9dbc0379b6a285ecd5
hamed <hamed.alizadegan@gmail.com>
2024-02-12 20:03:46 +0330

    category icon updated

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from accounts.models import User
 
 class HomeSettingModel(models.Model):
     name = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='images/header')
+    banner = models.ImageField(upload_to='images/header')
     header_logo = models.FileField(upload_to='images/home_setting/')
     footer_logo = models.FileField(upload_to='images/home_setting/')
     class_des = models.CharField(max_length=20, default='image_header')

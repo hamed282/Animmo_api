7c78a7ae119d069b7adf542a7b412ffe6fa0b2f0
hamed <hamed.alizadegan@gmail.com>
2024-03-03 17:29:36 +0330

    update setting

--- Animmo/urls.py
+++ Animmo/urls.py
@@ -18,6 +18,7 @@ urlpatterns = i18n_patterns(
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
     prefix_default_language=False)
 # ]
 

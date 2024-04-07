2597f0cf57fa99481175436cb4a71f6b214a9f85
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-04-07 20:40:12 +0330

    update

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import HomeSettingModel, FeedbackModel, GuideModel
+from .models import HomeSettingModel, FeedbackModel, GuideModel, HitsCountModel, TotalHitsModel
 
 
+class HitsCountModelAdmin(admin.ModelAdmin):
+    list_display = ['ip', 'mac', 'created', 'updated', 'count']
+
+
+class TotalHitsModelAdmin(admin.ModelAdmin):
+    list_display = ['date', 'hits']
+
+
+admin.site.register(HitsCountModel, HitsCountModelAdmin)
+admin.site.register(TotalHitsModel, TotalHitsModelAdmin)
 admin.site.register(HomeSettingModel)
 admin.site.register(FeedbackModel)
 admin.site.register(GuideModel)

3edae3a6f9a116613602d182f89d7d7a7e2cb1dc
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-04-17 09:54:25 +0330

    update

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ class User(AbstractBaseUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15, unique=True)
+    deaf = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
